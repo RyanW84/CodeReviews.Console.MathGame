a7c034a959b0492a54f1eb51e3884b54b8f5f107
Ryan W <ryanweavers@gmail.com>
2025-01-28 22:09:47 +0000

    fixed codacy issues
    fixed codacy issues

--- MathGameApp.RyanW84/MathGame/ScoreKeeper.cs
+++ MathGameApp.RyanW84/MathGame/ScoreKeeper.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MathGame
+﻿namespace MathGame
 {
     public class ScoreKeeper
     {
